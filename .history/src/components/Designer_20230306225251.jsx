2823a80766720110fa100b375944b28b81111000
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-03-06 23:02:10 +0000

    media query designer and fields section

--- .history/src/components/Designer_20230306225251.jsx
+++ .history/src/components/Designer_20230306225251.jsx
@@ -50,6 +50,32 @@ const Designer = () => {
 							massive background is now condensed into Source
 							Design. And it's everything just for you.
 						</p>
+						{!mobile ? (
+							<div className="buttonsDesigner">
+								{/* <a href={cv} download="cv.pdf"> */}
+
+								<div>
+									<AiOutlineLinkedin
+										className="icon"
+										size={40}
+										color={"#000000"}
+									/>
+								</div>
+								<div className="divlinkedin">
+									<a>Linkedin</a>
+								</div>
+								<div>
+									<TbFileDownload
+										className="icon"
+										size={40}
+										color={"#000000"}
+									/>
+								</div>
+								<div className="divresume">
+									<a>Resume</a>
+								</div>
+							</div>
+						) : null}
 					</div>
 				</div>
 
@@ -65,32 +91,7 @@ const Designer = () => {
 						))}
 					</div>
 				</div>
-				{!mobile ? (
-					<div className="buttonsDesigner">
-						{/* <a href={cv} download="cv.pdf"> */}
 
-						<div>
-							<AiOutlineLinkedin
-								className="icon"
-								size={40}
-								color={"#000000"}
-							/>
-						</div>
-						<div className="divlinkedin">
-							<a>Linkedin</a>
-						</div>
-						<div>
-							<TbFileDownload
-								className="icon"
-								size={40}
-								color={"#000000"}
-							/>
-						</div>
-						<div className="divresume">
-							<a>Resume</a>
-						</div>
-					</div>
-				) : null}
 				{mobile ? (
 					<div className="buttonsDesigner2">
 						{/* <a href={cv} download="cv.pdf"> */}
