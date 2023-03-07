9250ea232f4dfa18c16a1fded4165de04ef03a10
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-03-07 21:34:30 +0000

    footer, slider, latest events

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Contact.scss";
+import ContactForm from "./ContactForm";
+import contact from "../assets/contact.jpg";
 
 const Contact = () => {
-	return <div>Contact</div>;
+	return (
+		<section className="mainContact">
+			<div className="containerContact">
+				<h1>CONTACT</h1>
+				<h2>
+					You have got here, now drop me a line and let's have a chat.
+				</h2>
+				<div className="gridContact">
+					<div>
+						<ContactForm />
+					</div>
+					<div>
+						<img className="photoDesigner" src={contact} />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,69 @@
+import React, { useRef } from "react";
+import Button from "./Button";
+// import emailjs from "@emailjs/browser";
+import "./ContactForm.scss";
+
+const ContactForm = () => {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		// emailjs
+		// 	.sendForm(
+		// 		"service_9r2qy5t",
+		// 		"template_8hm6p3f2",
+		// 		form.current,
+		// 		"GwS-FbYtihip9wEJV"
+		// 	)
+		// 	.then(
+		// 		(result) => {
+		// 			alert("Thank you for your message! I will reply soon.");
+		// 			form.current.reset();
+		// 		},
+		// 		(error) => {
+		// 			alert("Error, try again.");
+		// 		}
+		// 	);
+	};
+
+	return (
+		<>
+			<section className="containerForm">
+				<div className="form">
+					<form ref={form} onSubmit={sendEmail}>
+						<label className="label" htmlFor="name">
+							Name
+						</label>
+						<input type="text" id="name" name="name" required />
+						<label className="label" htmlFor="email">
+							Email
+						</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							placeholder="email@email.com"
+							required
+						/>
+
+						<label className="label" htmlFor="message">
+							Message
+						</label>
+						<textarea
+							type="textarea"
+							id="message"
+							name="message"
+							rows="5"
+							placeholder="Write your message here"
+							required
+						/>
+						<Button name="Send" />
+					</form>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default ContactForm;
