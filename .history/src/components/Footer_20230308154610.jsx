93803934c3342043a6d0e7fca7b927487528433b
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-03-08 15:51:35 +0000

    media query updates

---
+++ .history/src/components/Footer_20230308154610.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Footer.scss";
+
+import { SiWhatsapp, SiInstagram } from "react-icons/si";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { RiMessengerLine } from "react-icons/ri";
+
+const Footer = () => {
+	return (
+		<div className="footerContainer">
+			<ul>
+				<li>
+					<a
+						href="https://m.me/lf.inocente"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<RiMessengerLine
+							className="icon"
+							size={60}
+							color={"#ffffff"}
+						/>
+					</a>
+				</li>
+				<li>
+					<a
+						href=" https://wa.me/+447916084770"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<SiWhatsapp
+							className="icon"
+							size={35}
+							color={"#ffffff"}
+						/>
+					</a>
+				</li>
+				<li>
+					<a className=""
+						href="https://www.instagram.com/lfi.design/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<SiInstagram size={35} color={"#ffffff"} />
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://www.linkedin.com/in/iiif/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<SlSocialLinkedin size={35} color={"#ffffff"} />
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Footer;
